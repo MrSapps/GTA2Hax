c321c25c2f33ae03faaee94ed897d8a8b503957e
Paul <Paul@paul.com>
2016-12-12 03:41:31 +0000

    Partial implementation of Vid_Init_SYS

--- DmaVideo/DmaVideo.cpp
+++ DmaVideo/DmaVideo.cpp
@@ -1,8 +1,321 @@
 #include "stdafx.h"
 #include "DmaVideo.hpp"
 
+#pragma comment(lib, "ddraw.lib")
+#pragma comment(lib, "dxguid.lib")
+
+static HINSTANCE gHinstance;
+static DWORD dword_100FFF8; // TODO: Reverse me
+
+using TDirectDrawEnumerateEx = decltype(&DirectDrawEnumerateExA);
+
+BOOL WINAPI DirectDrawEnumerateCallBack(
+    _In_ GUID FAR *lpGUID,
+    _In_ LPSTR    lpDriverDescription,
+    _In_ LPSTR    lpDriverName,
+    _In_ LPVOID   lpContext
+)
+{
+    SVideo* pVideoDriver = reinterpret_cast<SVideo*>(lpContext);
+    pVideoDriver->field_20_num_enums++;
+
+    const s32 driverDescStrLen = strlen(lpDriverDescription);
+    const s32 driverNameStrLen = strlen(lpDriverName);
+
+    const u32 requiredSize = driverDescStrLen + 1 + driverNameStrLen + 1 + sizeof(SDevice);
+    SDevice* pDevice = reinterpret_cast<SDevice*>(malloc(requiredSize));
+    memset(pDevice, 0, requiredSize);
+
+    pDevice->field_8_driver_name = reinterpret_cast<char*>(&pDevice->field_2C_end_buffer);
+    pDevice->field_C_driver_desc = reinterpret_cast<char*>(&pDevice->field_2C_end_buffer) + driverNameStrLen + 1;
+
+    memcpy(pDevice->field_8_driver_name, lpDriverName, driverNameStrLen + 1);
+    memcpy(pDevice->field_C_driver_desc, lpDriverDescription, driverDescStrLen + 1);
+
+    SDevice* pHead = pVideoDriver->field_30_device_info_last;
+    if (pHead)
+    {
+        pHead->field_10_next_ptr = pDevice;
+    }
+    else
+    {
+        pVideoDriver->field_2C_device_info_head_ptr = pDevice;
+    }
+    pVideoDriver->field_30_device_info_last = pDevice;
+
+    if (lpGUID)
+    {
+        pDevice->field_14_pDeviceGuid = &pDevice->field_18_guid;
+        pDevice->field_18_guid = *lpGUID;
+        pDevice->field_0_id = pVideoDriver->field_18_num_guids++;
+    }
+    else
+    {
+        pDevice->field_0_id = 1;
+    }
+
+    return TRUE;
+}
+
+BOOL WINAPI DirectDrawEnumerateExCallBack(
+    _In_ GUID FAR *lpGUID,
+    _In_ LPSTR    lpDriverDescription,
+    _In_ LPSTR    lpDriverName,
+    _In_ LPVOID   lpContext,
+    _In_ HMONITOR hm
+)
+{
+    // Pass to other call back
+    return DirectDrawEnumerateCallBack(lpGUID, lpDriverDescription, lpDriverName, lpContext);
+}
+
+SDevice* Init_DisplayMode_1001010(SDisplayMode* pDisplayMode, DDSURFACEDESC2* ddsurface, SVideo* pVideoDriver)
+{
+    pDisplayMode->field_8_width = ddsurface->dwWidth;
+    pDisplayMode->field_C_height = ddsurface->dwHeight;
+    pDisplayMode->field_14_rgb_bit_count = ddsurface->ddpfPixelFormat.dwRGBBitCount;
+    pDisplayMode->field_10_pitch = ddsurface->lPitch;
+    pDisplayMode->field_38_pnext = 0;
+
+    int bitNum = 0;
+    bool bIs32 = false;
+    auto result = ddsurface->ddpfPixelFormat.dwRBitMask;
+    
+    // Count number of bits
+    if (result & 1)
+    {
+        bIs32 = bitNum == 32;
+    }
+    else
+    {
+        for (;;)
+        {
+            bIs32 = bitNum == 32;
+            if (bitNum >= 32)
+            {
+                break;
+            }
+
+            result = result >> 1;
+            ++bitNum;
+
+            if (result & 1)
+            {
+                bIs32 = bitNum == 32;
+            }
+        }
+    }
+
+    if (bIs32)
+    {
+        pDisplayMode->field_18 = 0;
+        pDisplayMode->field_1C = 0;
+        pDisplayMode->field_28 = 0;
+        pDisplayMode->field_2C = 0;
+        pDisplayMode->field_20 = 0;
+        pDisplayMode->field_24 = 0;
+    }
+    else
+    {
+        // TODO
+        abort();
+    }
+    return nullptr;
+}
+
+HRESULT WINAPI EnumDisplayModesCallBack_1001340(
+    _In_ LPDDSURFACEDESC2 lpDDSurfaceDesc,
+    _In_ LPVOID           lpContext
+)
+{
+    SVideo* pVideoDriver = reinterpret_cast<SVideo*>(lpContext);
+    if (pVideoDriver->field_4_flags & 0x40)
+    {
+        // TODO: Debug/test code skipped
+    }
+
+    pVideoDriver->field_1C_num_display_modes++;
+    SDisplayMode* pDisplayMode = new SDisplayMode();
+    memset(pDisplayMode, 0, sizeof(SDisplayMode));
+
+    if (pVideoDriver->field_28_display_mode_array)
+    {
+        pVideoDriver->field_28_display_mode_array[14] = pDisplayMode;
+    }
+    else
+    {
+        pVideoDriver->field_24_head_ptr = pDisplayMode;
+    }
+
+    pVideoDriver->field_28_display_mode_array = (SDisplayMode **)pDisplayMode; // TODO: Probably wrong
+    pDisplayMode->field_0_display_mode_idx = pVideoDriver->field_14_display_mode_count_2_q;
+    ++pVideoDriver->field_14_display_mode_count_2_q;
+    pDisplayMode->field_3C = 1;
+    pDisplayMode->field_4_deviceId = pVideoDriver->field_34_active_device_id;
+    Init_DisplayMode_1001010(pDisplayMode, lpDDSurfaceDesc, 0);
+
+    return DDENUMRET_OK;
+}
+
 SVideo* CC Vid_Init_SYS(s32 param1, u16 param2_flags)
 {
+    HMODULE hDirectDraw = ::LoadLibraryA("ddraw.dll");
+    if (hDirectDraw)
+    {
+        ::FreeLibrary(hDirectDraw);
+        SVideo* pVideoDriver = new SVideo();
+        memset(pVideoDriver, 0, sizeof(SVideo));
+        pVideoDriver->field_7C = gHinstance;
+        pVideoDriver->field_84_from_initDLL = dword_100FFF8;
+        pVideoDriver->field_0 = 1;
+        pVideoDriver->field_78 = param1;
+        pVideoDriver->field_14_display_mode_count_2_q = 1;
+        pVideoDriver->field_18_num_guids = 2;
+        pVideoDriver->field_4_flags = param2_flags & 0x40 | 0x200;
+        
+        TDirectDrawEnumerateEx pDirectDrawEnumerateEx = nullptr;
+        
+        if (param2_flags & 4)
+        {
+            // Yes - using a freed HMODULE is correct here! Mainly because this dll linked to ddraw so its not actually unloaded
+            pDirectDrawEnumerateEx = reinterpret_cast<TDirectDrawEnumerateEx>(::GetProcAddress(hDirectDraw, "DirectDrawEnumerateEx"));
+        }
+        if (pDirectDrawEnumerateEx)
+        {
+            pVideoDriver->field_88_last_error = pDirectDrawEnumerateEx(DirectDrawEnumerateExCallBack, pVideoDriver, 7); // TODO: Constant
+        }
+        else
+        {
+            pVideoDriver->field_88_last_error = DirectDrawEnumerateA(DirectDrawEnumerateCallBack, pVideoDriver);
+        }
+
+        if (pVideoDriver->field_88_last_error)
+        {
+            delete pVideoDriver;
+            pVideoDriver = nullptr;
+        }
+
+      
+        if (param2_flags & 8 || (pVideoDriver && !pVideoDriver->field_2C_device_info_head_ptr))
+        {
+            // TODO: Might be wrong conditional
+            return pVideoDriver;
+        }
+        else
+        {
+            auto pDeviceInfo = pVideoDriver->field_2C_device_info_head_ptr;
+            auto pNextDevice = pVideoDriver->field_2C_device_info_head_ptr;
+
+            auto pDD7 = &pVideoDriver->field_8C_DirectDraw7;
+            auto ppDD4 = &pVideoDriver->field_120_IDDraw4;
+            auto ppDD7Copy = &pVideoDriver->field_8C_DirectDraw7;
+
+            for (;;)
+            {
+                pVideoDriver->field_34_active_device_id = pDeviceInfo->field_0_id;
+                auto lpGUID = pDeviceInfo->field_14_pDeviceGuid;
+                if (*ppDD4)
+                {
+                    //--gDD4Refs_dword_100FFF4;
+                    (*ppDD4)->Release();
+                    *ppDD4 = 0;
+                }
+
+                if (*pDD7)
+                {
+                    //--gDD7Refs_dword_100FFF0;
+                    (*pDD7)->Release();
+                    *pDD7 = 0;
+                }
+
+                // Create DDraw7 Instance
+                pVideoDriver->field_88_last_error = DirectDrawCreate(lpGUID, (LPDIRECTDRAW *)pDD7, 0);
+                //++gDD7Refs_dword_100FFF0;
+                if (pVideoDriver->field_88_last_error)
+                {
+                    break;
+                }
+
+                // Query DDraw4 instance from 7
+                pVideoDriver->field_88_last_error = (*pDD7)->QueryInterface(
+                    IID_IDirectDraw4,
+                    (LPVOID *)&pVideoDriver->field_120_IDDraw4);
+                //++gDD4Refs_dword_100FFF4;
+
+                if (pVideoDriver->field_88_last_error)
+                {
+                    (*pDD7)->Release();
+                    *pDD7 = 0;
+                    return 0;
+                }
+
+                memset(&pVideoDriver->field_1C8_device_caps, 0, sizeof(DDCAPS));
+                memset(&pVideoDriver->field_344_hel_caps, 0, sizeof(DDCAPS));
+                auto pDD4Copy = *ppDD4;
+                pVideoDriver->field_1C8_device_caps.dwSize = sizeof(DDCAPS);
+                pVideoDriver->field_344_hel_caps.dwSize = sizeof(DDCAPS);
+                pVideoDriver->field_88_last_error = pDD4Copy->GetCaps(
+                    &pVideoDriver->field_1C8_device_caps,
+                    &pVideoDriver->field_344_hel_caps);
+      
+                if (pVideoDriver->field_1C8_device_caps.dwCaps2 & 0x80000)
+                {
+                    pNextDevice->field_4_flags |= 1;
+                }
+
+                pNextDevice->field_28_dwVidMemTotal = pVideoDriver->field_1C8_device_caps.dwVidMemTotal;
+                auto pDD4 = *ppDD4;
+                if (pVideoDriver->field_88_last_error)
+                {
+                    if (pDD4)
+                    {
+                        //--gDD4Refs_dword_100FFF4;
+                        (*ppDD4)->Release();
+                        *ppDD4 = 0;
+                    }
+                    if (*ppDD7Copy)
+                    {
+                        //--gDD7Refs_dword_100FFF0;
+                        (*ppDD7Copy)->Release();
+                        *ppDD7Copy = 0;
+                    }
+                    break;
+                }
+
+                pVideoDriver->field_88_last_error = pDD4->EnumDisplayModes(
+                    0,
+                    0,
+                    pVideoDriver,
+                    EnumDisplayModesCallBack_1001340);
+
+                pVideoDriver->field_34_active_device_id = 0;
+                if (ppDD4)
+                {
+                    //--gDD4Refs_dword_100FFF4;
+                    (*ppDD4)->Release();
+                    *ppDD4 = 0;
+                }
+
+                if (*ppDD7Copy)
+                {
+                    //--gDD7Refs_dword_100FFF0;
+                    (*ppDD7Copy)->Release();
+                    *ppDD7Copy = 0;
+                }
+
+                pNextDevice = pNextDevice->field_10_next_ptr;
+                pDeviceInfo = pNextDevice;
+                
+                if (!pNextDevice)
+                {
+                    // TODO: Check correct
+                    return pVideoDriver;
+                }
+                pDD7 = &pVideoDriver->field_8C_DirectDraw7;
+            }
+        }
+
+    }
     return nullptr;
 }
 
@@ -103,6 +416,8 @@ s32 CC Vid_WindowProc(SVideo* pVideoDriver, HWND hwnd, DWORD uMsg, WPARAM wParam
 
 s32 CC Vid_InitDLL(HINSTANCE hInstance, s32 a2)
 {
+    gHinstance = hInstance;
+    dword_100FFF8 = a2;
     return 0;
 }
 
--- DmaVideo/DmaVideo.hpp
+++ DmaVideo/DmaVideo.hpp
@@ -1,21 +1,137 @@
 #pragma once
 
 #include <windows.h>
+#include <ddraw.h>
 
 struct SVideo
 {
-
+    DWORD field_0;
+    DWORD field_4_flags;
+    DWORD field_8_found_width;
+    DWORD field_C_found_height;
+    DWORD field_10_found_rgb_bit_count;
+    DWORD field_14_display_mode_count_2_q;
+    DWORD field_18_num_guids;
+    DWORD field_1C_num_display_modes;
+    DWORD field_20_num_enums;
+    struct SDisplayMode* field_24_head_ptr;
+    struct SDisplayMode** field_28_display_mode_array;
+    struct SDevice* field_2C_device_info_head_ptr;
+    struct SDevice* field_30_device_info_last;
+    DWORD field_34_active_device_id;
+    DWORD field_38;
+    void* field_3C_current_enum_ptr;
+    DWORD field_40_minus2IfHaveSurface;
+    DWORD field_44;
+    DWORD field_48_rect_right;
+    DWORD field_4C_rect_bottom;
+    DWORD field_50_surface_pixels_ptr;
+    DWORD field_54_surface_pixels_pitch;
+    DWORD field_58;
+    DWORD field_5C;
+    DWORD field_60_g;
+    DWORD field_64_r;
+    DWORD field_68_b;
+    DWORD field_6C;
+    DWORD field_70;
+    DWORD field_74;
+    DWORD field_78;
+    HINSTANCE field_7C;
+    DWORD field_80_active_mode_q;
+    DWORD field_84_from_initDLL;
+    DWORD field_88_last_error;
+    IDirectDraw7* field_8C_DirectDraw7;
+    DWORD field_90;
+    DWORD field_94;
+    DWORD field_98;
+    DWORD field_9C;
+    DWORD field_A0;
+    DWORD field_A4;
+    DWORD field_A8;
+    DWORD field_AC;
+    DWORD field_B0;
+    DWORD field_B4;
+    DWORD field_B8;
+    DWORD field_BC;
+    DWORD field_C0;
+    DWORD field_C4;
+    DWORD field_C8;
+    DWORD field_CC;
+    DWORD field_D0;
+    DWORD field_D4;
+    DWORD field_D8;
+    DWORD field_DC;
+    DWORD field_E0;
+    DWORD field_E4;
+    DWORD field_E8;
+    DWORD field_EC;
+    DWORD field_F0;
+    DWORD field_F4;
+    DWORD field_F8;
+    DWORD field_FC;
+    DWORD field_100;
+    DWORD field_104;
+    DWORD field_108;
+    DWORD field_10C;
+    DWORD field_110;
+    DWORD field_114;
+    DWORD field_118;
+    DWORD field_11C;
+    IDirectDraw4* field_120_IDDraw4;
+    DWORD field_124;
+    DWORD field_128;
+    DWORD field_12C;
+    DWORD field_130;
+    void* field_134_SurfacePrimary;
+    void* field_138_Surface;
+    DDSURFACEDESC2 field_13C_DDSurfaceDesc7;
+    DWORD field_1B8_clipper;
+    DWORD field_1BC;
+    DWORD field_1C0;
+    DWORD field_1C4;
+    DDCAPS field_1C8_device_caps;
+    DDCAPS field_344_hel_caps;
+    HWND field_4C0_hwnd;
 };
+
+static_assert(sizeof(DDCAPS) == 0x17C, "Wrong sized DDCAPS");
+static_assert(sizeof(DDSURFACEDESC2) == 0x7C, "Wrong sized DDSURFACEDESC2");
+static_assert(sizeof(SVideo) == 0x4C4, "Wrong sized SVideo");
 
 struct SDevice
 {
-
+    DWORD field_0_id;
+    DWORD field_4_flags;
+    char* field_8_driver_name;
+    char* field_C_driver_desc;
+    SDevice* field_10_next_ptr;
+    GUID* field_14_pDeviceGuid;
+    GUID field_18_guid;
+    DWORD field_28_dwVidMemTotal;
+    DWORD field_2C_end_buffer; // TODO: Actually a word and SDevice is 0x2E ?
 };
+static_assert(sizeof(SDevice) == 0x30, "Wrong sized SDevice");
 
 struct SDisplayMode
 {
-
+    DWORD field_0_display_mode_idx;
+    DWORD field_4_deviceId;
+    DWORD field_8_width;
+    DWORD field_C_height;
+    DWORD field_10_pitch;
+    DWORD field_14_rgb_bit_count;
+    DWORD field_18;
+    DWORD field_1C;
+    DWORD field_20;
+    DWORD field_24;
+    DWORD field_28;
+    DWORD field_2C;
+    DWORD field_30;
+    DWORD field_34;
+    DWORD field_38_pnext;
+    DWORD field_3C;
 };
+static_assert(sizeof(SDisplayMode) == 0x40, "Wrong sized SDisplayMode");
 
 struct SVidVersion
 {
@@ -25,6 +141,7 @@ struct SVidVersion
 
 using u16 = unsigned short int;
 using s32 = signed int;
+using u32 = unsigned int;
 using f32 = float;
 using u8 = unsigned char;
 
